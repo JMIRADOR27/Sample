c118a171c04c271085bf95d9a6bb20ed72497be8
Renzo <renzo.amoranto@coexstar.ph>
2021-12-07 16:02:25 +0800

    Updates

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 import DataTable from './DataTable';
 
 export default class ReactDataTableApp extends Component {
-  constructor(props) {
-    super(props);
-  }
+
 
   render() {
-    const columns = ['id', 'name', 'email', 'address', 'created_at'];
+    const columns = ['id', 'name', 'email', 'created_at'];
     return (
       <DataTable url="/api/user" columns={columns} />
     );
